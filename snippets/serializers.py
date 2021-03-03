6b9c7733dc521838ee2d421f700adf8c4a61bcbd
Wolodia <Riabow>
2021-03-03 13:56:25 +0300

    snippet list

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,6 +2,18 @@ from rest_framework import serializers
 from snippets.models import *
 
 
+class SnippetSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'title', 'code', 'linenos', 'language', 'style']
+
+    """
+Первый вариант: используем класс Serializer
+    Суть его отличия от класса ModelSerializer такая же, как в случае
+    Form и ModelForm. Первый вариант более громоздкий, но более гибкий
+    в настройке. 
+
+
 class SnippetSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField(
@@ -17,15 +29,15 @@ class SnippetSerializer(serializers.Serializer):
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
 
     def create(self, validated_data):
-        """
+        
         Create and return a new `Snippet` instance, given the validated data.
-        """
+        
         return Snippet.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
-        """
+        
         Update and return an existing `Snippet` instance, given the validated data.
-        """
+        
         instance.title = validated_data.get('title', instance.title)
         instance.code = validated_data.get('code', instance.code)
         instance.linenos = validated_data.get('linenos', instance.linenos)
@@ -33,5 +45,4 @@ class SnippetSerializer(serializers.Serializer):
         instance.style = validated_data.get('style', instance.style)
         instance.save()
         return instance
-
-
+    """
--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from django.http import HttpResponse, JsonResponse
+from rest_framework.parsers import JSONParser
+from django.views.decorators.csrf import csrf_exempt  # bypasses csrf token validation
 
-# Create your views here.
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    Вью-функция отвечающая за отображение всех экземпляров класса Snippet
+    и за создание новых экземпляров.
+
+    :param request:
+    :return:
+    """
+    if request.method == 'GET':
+        snippets = Snippet.objects.all()
+        serializer = SnippetSerializer(snippets, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
