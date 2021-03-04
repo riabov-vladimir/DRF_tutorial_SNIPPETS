1f5f0a840b578d35ebc8557db49086a2c59e63f4
Wolodia <Riabow>
2021-03-04 15:20:50 +0300

    CBV

--- snippets/urls.py
+++ snippets/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from snippets.views import *
 
 urlpatterns = [
-    path('snippets/', snippet_list, name='no_pk_views'),
-    path('snippets/<pk>/', snippet_detail, name='pk_views')
+    path('snippets/', SnippetList.as_view(), name='no_pk_views'),
+    path('snippets/<pk>/', SnippetDetail.as_view(), name='pk_views')
+    # path('snippets/', snippet_list, name='no_pk_views'),
+    # path('snippets/<pk>/', snippet_detail, name='pk_views')
 ]
--- snippets/views.py
+++ snippets/views.py
@@ -1,6 +1,6 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, JsonResponse, Http404
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt  # bypasses csrf token validation
 from rest_framework.views import APIView
@@ -8,25 +8,22 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+#
+#                Вариант 3
+#                - используем class-based-views
+#                - всё выглядит чуть более элегантно
+#                - очень понравилась общая проверка наличия инстанса get_object
 
-#        Вариант второй
-#        - вводим объект Response, который умеет проверять типы
-#        даных и работать с набором именованных кодов
-#        - вводим декоратор отвечающий за методы запросов
-
-
-@api_view(['GET', 'POST'])
-def snippet_list(request):
+class SnippetList(APIView):
     """
-    Вью-функция отвечающая за отображение всех экземпляров класса Snippet
-    и за создание новых экземпляров.
+    List all snippets, or create a new snippet.
     """
-    if request.method == 'GET':
+    def get(self, request, format=None):
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    elif request.method == 'POST':
+    def post(self, request, format=None):
         serializer = SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -34,34 +31,95 @@ def snippet_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
-    """
-    Вью-функция отвечающая за отображение, удаление и обновление конкретного
-    экземпляра класса Snippet с идентификатором равным PK.
-    """
-    try:
-        snippet = Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class SnippetDetail(APIView):
 
-    if request.method == 'GET':
+    def get_object(self, pk):
+        """
+        Получить экземпляр класса Snippet из базы данных по первичному ключу
+        или поднять ошибку "404"
+        :param pk: 'id'
+        :return:
+        """
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method == 'PUT':
+    def put(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         serializer = SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+            return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'DELETE':
+    def delete(self, request, pk, format=None):
+        snippet = self.get_object(pk)
         snippet.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
+
+# #        Вариант второй
+# #        - вводим объект Response, который умеет проверять типы
+# #        данных и работать с набором именованных кодов
+# #        - вводим декоратор отвечающий за методы запросов
+#
+#
+# @api_view(['GET', 'POST'])
+# def snippet_list(request):
+#     """
+#     Вью-функция отвечающая за отображение всех экземпляров класса Snippet
+#     и за создание новых экземпляров.
+#     """
+#     if request.method == 'GET':
+#         snippets = Snippet.objects.all()
+#         serializer = SnippetSerializer(snippets, many=True)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'POST':
+#         serializer = SnippetSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def snippet_detail(request, pk):
+#     """
+#     Вью-функция отвечающая за отображение, удаление и обновление конкретного
+#     экземпляра класса Snippet с идентификатором равным PK.
+#     """
+#     try:
+#         snippet = Snippet.objects.get(pk=pk)
+#     except Snippet.DoesNotExist:
+#         return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     if request.method == 'GET':
+#         serializer = SnippetSerializer(snippet)
+#         return Response(serializer.data)
+#
+#     elif request.method == 'PUT':
+#         serializer = SnippetSerializer(snippet, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     elif request.method == 'DELETE':
+#         snippet.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 #           Первый базовый вариант -- ВЬЮ-ФУНКЦИИ
+#           - по сути никакие особенности REST-framework'a тут не
+#             используются, всё делается как в обычном DJANGO
 
 
 # @csrf_exempt
